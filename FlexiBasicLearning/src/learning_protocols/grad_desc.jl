4ebd4e3a109a839fa6912dd47ed0ec6face6772f
xialu <stilese@uci.edu>
2026-07-15 11:06:52 -0700

    added slow kwarg to gd(). if slow track grads, params

--- FlexiBasicLearning/src/learning_protocols/grad_desc.jl
+++ FlexiBasicLearning/src/learning_protocols/grad_desc.jl
@@ -2,7 +2,7 @@
 using Optimization
 using OptimizationOptimJL
 
-function gradient_descent_learn(learning_problem, ig; maxiters=10000)
+function gradient_descent_learn(learning_problem, ig; maxiters=10000, slow = false)
 
     function flexi_loss(params, p)
         return get_loss(params; learning_problem=learning_problem, gradient_mode=true)
@@ -10,14 +10,24 @@ function gradient_descent_learn(learning_problem, ig; maxiters=10000)
 
     loss_history = Float64[]
     grad_norm_history = Float64[]
+    if slow
+        grads = []
+        params = []
+    end
     config = CallbackConfig()
     
     function callback(p, lossval)
         push!(loss_history, lossval)
+        current_iter = length(loss_history)
+
         if hasproperty(p, :grad) && !isnothing(p.grad)
             push!(grad_norm_history, norm(p.grad))
+            if slow && current_iter % config.print_frequency == 0
+                push!(grads, p.grad)
+                push!(params, p.u)
+            end
         end
-        current_iter = length(loss_history)
+
         if config.verbose && current_iter % config.print_frequency == 0
             qdrms = sqrt(lossval / config.constants.qdrms_divisor)
             println("In grad_descent, iteration $current_iter: loss=$lossval, qdrms=$qdrms at $(now())")
@@ -32,5 +42,10 @@ function gradient_descent_learn(learning_problem, ig; maxiters=10000)
     # could try other gradient descent optimizers (BFGS)
 
     println("Final loss: $(sol.objective)")
+
+    if slow
+        return sol.u, loss_history, grad_norm_history, grads, params
+    end
     return sol.u, loss_history, grad_norm_history
+    
 end
